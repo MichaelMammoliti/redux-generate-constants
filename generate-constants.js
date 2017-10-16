1b51fd2b836158f33831bdab10e265e70409d390
Michael Mammoliti <mammoliti.michael@gmail.com>
2017-10-16 10:29:25 +0100

    Update generate-constants.js

--- generate-constants.js
+++ generate-constants.js
@@ -1,24 +1,23 @@
 const generateConstants = ({ prefix, constants }) => {
   const newConstants = {};
-  let prefixString;
-  
+
   if (!prefix && !constants) {
-    console.warn('no options provided to generateConstants');
-    
-    return newConstants;
-  }
-  
-  if (!Array.isArray(constants) || !constants.length) {
-    console.warn(`unavailable constants or wrong type: constant's type is "${typeof constants}", expected array.`);
+    console.warn('no options provided to generateConstants'); // eslint-disable-line
 
     return newConstants;
   }
-  
-  if (!prefix || typeof prefix !== 'string') {
-    console.warn('prefix is not a string, no prefix will be applied to the constants');
-    
-    prefixString = (typeof prefix === 'string') ? `${prefix}/` : '';
+
+  if (!Array.isArray(constants) || !constants.length) {
+    console.warn(`unavailable constants or wrong type: constant's type is "${typeof constants}", expected array.`); // eslint-disable-line
+
+    return newConstants;
   }
+
+  if (!prefix || typeof prefix !== 'string') {
+    console.warn('prefix is not a string, no prefix will be applied to the constants'); // eslint-disable-line
+  }
+
+  const prefixString = (prefix && typeof prefix === 'string') ? `${prefix}/` : '';
 
   constants.forEach(constantString => {
     newConstants[constantString] = `${prefixString}${constantString}`;
